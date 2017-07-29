a83c9a8d3849fba4dcea46af3660f9b96296f4fe
Danilo Gonzalez <gonzalezantonio.danilo@gmail.com>
2017-07-28 19:48:59 -0700

    Included Bootstrap and tried implementing routes.

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import NavigationBar from "./NavigationBar";
+import Home from "./Home";
 
 class App extends Component{
   render(){
     return (
       <div>
-        <h1>Testing App</h1>
+        <NavigationBar />
+        <Home />
       </div>
     );
   }
