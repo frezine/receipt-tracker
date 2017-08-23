a92d2a27fe584f287eb632bdce547c3aa0718d6e
Danilo Gonzalez <gonzalezantonio.danilo@gmail.com>
2017-08-23 01:09:21 -0700

    Fixing merge conflicts

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -8,52 +8,28 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
-      category: "",
-      user_id: this.props.location.state.userid,
-      category_id: "",
-      make_new_receipt: 0,
-      state_change:true
-=======
       user_id: this.props.location.state.user_id,
       group: "",
       group_id: "",
       new_receipt: false
->>>>>>> 1923c57d39d090fc7855c209f140edc93c36cd52
     }
     this.onClick = this.onClick.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
   }
 
-<<<<<<< HEAD
-  clickNewReceipt() {
-    this.setState({make_new_receipt: true});
-  }
-
-  setReceiptCategory(e){
-    e.preventDefault()
-    this.setState({ [e.target.name]: e.target.value });
-=======
   onClick() {
     this.setState({
       new_receipt: true
     });
->>>>>>> 1923c57d39d090fc7855c209f140edc93c36cd52
   }
 
   onSubmit(e){
     e.preventDefault();
-<<<<<<< HEAD
-    this.setState({make_new_receipt: false});
-    console.log("chancing state");
-    this.makeCategory();
-=======
     this.setState({
       new_receipt: false
     });
     this.createGroup();
->>>>>>> 1923c57d39d090fc7855c209f140edc93c36cd52
   }
 
   onChange(e){
@@ -65,15 +41,10 @@ class Dashboard extends Component {
     axios.post("/api/groups", this.state)
     .then(
       (res) => {
-<<<<<<< HEAD
-        this.setState({category_id: res.data._id});
-        this.associateCategory();
-=======
         console.log("Added group: " + res);
         this.setState({group_id: res.data._id});
         console.log("this is the group_id" + this.state.group_id);
         this.associateGroup();
->>>>>>> 1923c57d39d090fc7855c209f140edc93c36cd52
       },
       (err) => {
         console.log(err);
@@ -81,14 +52,9 @@ class Dashboard extends Component {
     );
   }
 
-<<<<<<< HEAD
-  associateCategory() {
-    axios.post("/api/receiptUserCategory", this.state)
-=======
   associateGroup() {
     console.log("associating user with group");
     axios.post("/api/associateUserGroup", this.state)
->>>>>>> 1923c57d39d090fc7855c209f140edc93c36cd52
     .then(
       (res) => {
         console.log('added group');
@@ -99,21 +65,11 @@ class Dashboard extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        <DisplayCategorySideBar
-          categoryID={this.state.user_id}
-          stateChange={this.state.state_change}
-        />
-        <button id='b1'
-          style={{fontSize: 20, color: 'green'}}
-          onClick={this.clickNewReceipt}>
-=======
         {/* <DisplayCategorySideBar
           user_id={this.state.user_id}
         /> */}
         <button
           onClick={this.onClick}>
->>>>>>> 1923c57d39d090fc7855c209f140edc93c36cd52
           Get!
         </button>
         {this.state.new_receipt &&
--- client/src/components/UserSideBar/DisplayCategorySideBar.js
+++ client/src/components/UserSideBar/DisplayCategorySideBar.js
@@ -1,17 +1,13 @@
 import axios from "axios";
 import React, { Component } from "react";
-<<<<<<< HEAD:client/src/components/Category/DisplayCategorySideBar.js
 import { Link, Route, Redirect, Switch } from "react-router-dom";
-=======
->>>>>>> 1923c57d39d090fc7855c209f140edc93c36cd52:client/src/components/UserSideBar/DisplayCategorySideBar.js
 import PropTypes from "prop-types";
 
 class DisplayCategorySideBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD:client/src/components/Category/DisplayCategorySideBar.js
-      _id: this.props.categoryID,
+      _id: this.props.user_id,
       stateChange: this.props.stateChange,
       category_id_list: [],
       category_dict: {},
@@ -21,12 +17,6 @@ class DisplayCategorySideBar extends Component {
     this.getCategory = this.getCategory.bind(this);
     this.getCategoryName = this.getCategoryName.bind(this);
     this.redirectGroupPage = this.redirectGroupPage.bind(this);
-=======
-      _id: this.props.user_id,
-      category_id_list: this.props.category_id_list,
-      category_dict: {}
-    }
->>>>>>> 1923c57d39d090fc7855c209f140edc93c36cd52:client/src/components/UserSideBar/DisplayCategorySideBar.js
   }
 
   componentWillMount() {
@@ -34,16 +24,10 @@ class DisplayCategorySideBar extends Component {
     this.getCategory();
  }
 
-
   //map the id with the categoryName and put it in a dictionary
-<<<<<<< HEAD:client/src/components/Category/DisplayCategorySideBar.js
   getCategoryName() {
     console.log("in get category name");
     var dictionary = {}
-=======
-  getGroupNames() {
-    let dictionary = {}
->>>>>>> 1923c57d39d090fc7855c209f140edc93c36cd52:client/src/components/UserSideBar/DisplayCategorySideBar.js
     this.state.category_id_list.map(function(category_id_list, index){
       axios.get("/api/categoryNameById?_id=" + category_id_list)
       .then(
@@ -74,7 +58,6 @@ class DisplayCategorySideBar extends Component {
     );
   }
 
-<<<<<<< HEAD:client/src/components/Category/DisplayCategorySideBar.js
   redirectGroupPage(key) {
     console.log("inside redirect gorup page");
     console.log(key);
@@ -85,14 +68,6 @@ class DisplayCategorySideBar extends Component {
 
   render() {
     const categoryIdDict = this.state.category_dict;
-=======
-  componentWillMount(){
-    this.getGroupNames();
-  }
-
-  render() {
-    console.log("hello2");
->>>>>>> 1923c57d39d090fc7855c209f140edc93c36cd52:client/src/components/UserSideBar/DisplayCategorySideBar.js
     return (
         <div>
           {
@@ -117,13 +92,9 @@ class DisplayCategorySideBar extends Component {
 
 DisplayCategorySideBar.propTypes = {
   //categoryid is suppose to be user id
-  categoryID: PropTypes.string.isRequired,
-<<<<<<< HEAD:client/src/components/Category/DisplayCategorySideBar.js
-  stateChange: PropTypes.string.isRequired
-=======
+  user_id: PropTypes.string.isRequired,
   category_dict: PropTypes.object,
   category_id_list: PropTypes.array
->>>>>>> 1923c57d39d090fc7855c209f140edc93c36cd52:client/src/components/UserSideBar/DisplayCategorySideBar.js
 };
 
 export default DisplayCategorySideBar;
